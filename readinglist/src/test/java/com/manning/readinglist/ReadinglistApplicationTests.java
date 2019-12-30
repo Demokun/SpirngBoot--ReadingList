3a36d2b88265678e9e8609c77d514f163fc840b5
Demo <1012858388@qq.com>
2019-12-30 16:59:47 +0800

    创建

---
+++ readinglist/src/test/java/com/manning/readinglist/ReadinglistApplicationTests.java
@@ -0,0 +1,16 @@
+/*
+	一个基本的集成测试类
+ */
+package com.manning.readinglist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReadinglistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
